77213ff61beddc69cbf72bad5e0eb845c887d30c
karanshah197 <karanshah197@gmail.com>
2019-10-07 00:08:34 -0400

    Initial Spring boot setup

---
+++ src/main/java/com/eml/energy/EnergyApplication.java
@@ -0,0 +1,13 @@
+package com.eml.energy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergyApplication.class, args);
+	}
+
+}
