8b281889eea4a9e6a64c57771352084157a7d827
karanshah197 <karanshah197@gmail.com>
2019-11-03 19:00:47 -0500

    Sample unit test

--- src/main/java/com/eml/energy/EnergyApplication.java
+++ src/main/java/com/eml/energy/EnergyApplication.java
@@ -8,10 +8,12 @@ package com.eml.energy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
+//@EnableJpaAuditing
+//@Import({ApplicationConfig.class })
 public class EnergyApplication {
 
 	public static void main(String[] args) {
