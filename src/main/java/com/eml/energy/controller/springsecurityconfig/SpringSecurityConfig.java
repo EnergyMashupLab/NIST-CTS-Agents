25669ca96647b30cc003d44407d45fcdc6e65498
karanshah197 <karanshah197@gmail.com>
2019-11-06 22:00:34 -0500

    Fixing the login redirection

--- src/main/java/com/eml/energy/controller/springsecurityconfig/SpringSecurityConfig.java
+++ src/main/java/com/eml/energy/controller/springsecurityconfig/SpringSecurityConfig.java
@@ -1,11 +1,18 @@
 package com.eml.energy.controller.springsecurityconfig;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+@Configuration
+@EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     // Create 2 users for demo
     @Override
@@ -29,14 +36,36 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 .httpBasic()
                 .and()
                 .authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/tenders/**").hasRole("USER")
-                .antMatchers(HttpMethod.POST, "/tenders").hasRole("ADMIN")
-                .antMatchers(HttpMethod.PUT, "/books/**").hasRole("ADMIN")
-                .antMatchers(HttpMethod.PATCH, "/books/**").hasRole("ADMIN")
-                .antMatchers(HttpMethod.DELETE, "/books/**").hasRole("ADMIN")
+                /*.antMatchers(HttpMethod.GET, "/tenders/**").hasRole("USER")
+                .antMatchers(HttpMethod.POST, "/tenders/**").hasRole("USER")
+                .antMatchers(HttpMethod.DELETE, "/tenders/**").hasRole("USER")
+                .antMatchers(HttpMethod.GET, "/canTen/**").hasRole("USER")
+                .antMatchers(HttpMethod.POST, "/canTen/**").hasRole("USER")
+                .antMatchers(HttpMethod.DELETE, "/canTen/**").hasRole("USER")
+                .antMatchers(HttpMethod.GET, "/transaction/**").hasRole("USER")
+                .antMatchers(HttpMethod.POST, "/transaction/**").hasRole("USER")
+                .antMatchers(HttpMethod.DELETE, "/transaction/**").hasRole("USER")
+                .antMatchers(HttpMethod.GET, "/reqtend/**").hasRole("USER")
+                .antMatchers(HttpMethod.POST, "/reqtend/**").hasRole("USER")
+                .antMatchers(HttpMethod.DELETE, "/reqtend/**").hasRole("USER")
+                .antMatchers(HttpMethod.GET, "/reqtrans/**").hasRole("USER")
+                .antMatchers(HttpMethod.POST, "/reqtrans/**").hasRole("USER")
+                .antMatchers(HttpMethod.DELETE, "/reqtrans/**").hasRole("USER")*/
+                .antMatchers(HttpMethod.GET, "/testTender/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.POST, "/testTender/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/testTender/**").hasRole("ADMIN")
                 .and()
-                .csrf().disable();
-                //.formLogin().disable();]]=
+                .csrf().disable()
+                //.formLogin().disable();]
+                .formLogin()
+                .loginPage("/login.html")
+                .loginProcessingUrl("/perform_login")
+                .defaultSuccessUrl("/index.html", true)
+                .and()
+                .logout()
+                .logoutUrl("/perform_logout")
+                .deleteCookies("JSESSIONID")
+        ;
 
         /*
         http
@@ -72,8 +101,8 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
         User.UserBuilder users = User.withDefaultPasswordEncoder();
 
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(users.username("user").password("password").roles("USER").build());
-        manager.createUser(users.username("admin").password("password").roles("USER", "ADMIN").build());
+        manager.createUser(users.username("kvs32").password("password").roles("USER").build());
+        manager.createUser(users.username("karan").password("password").roles("USER", "ADMIN").build());
         return manager;
     }*/
 }
