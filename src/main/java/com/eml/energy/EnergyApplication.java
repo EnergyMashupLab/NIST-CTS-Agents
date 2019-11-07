87bc0c436b2e63bffa50162cc2e1588c17daf935
karanshah197 <karanshah197@gmail.com>
2019-11-06 20:58:47 -0500

    Added Spring Security Core plugin

--- src/main/java/com/eml/energy/EnergyApplication.java
+++ src/main/java/com/eml/energy/EnergyApplication.java
@@ -1,8 +1,9 @@
-/**
+/*
  * @author Karan Shah
  * @version 1.0
  * @created 10-Oct-2019
- */
+ *
+*/
 
 package com.eml.energy;
 
@@ -21,4 +22,12 @@ public class EnergyApplication {
 		SpringApplication.run(EnergyApplication.class, args);
 	}
 
+	/*@Bean
+	CommandLineRunner initDatabase(BookRepository repository) {
+		return args -> {
+			repository.save(new Book("A Guide to the Bodhisattva Way of Life", "Santideva", new BigDecimal("15.41")));
+			repository.save(new Book("The Life-Changing Magic of Tidying Up", "Marie Kondo", new BigDecimal("9.69")));
+			repository.save(new Book("Refactoring: Improving the Design of Existing Code", "Martin Fowler", new BigDecimal("47.99")));
+		};
+	}*/
 }
---
+++ src/main/java/com/eml/energy/controller/springsecurityconfig/SpringSecurityConfig.java
@@ -0,0 +1,79 @@
+package com.eml.energy.controller.springsecurityconfig;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+    // Create 2 users for demo
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.inMemoryAuthentication()
+                .withUser("user").password("{noop}password").roles("USER")
+                .and()
+                .withUser("admin").password("{noop}password").roles("USER", "ADMIN");
+
+    }
+
+    // Secure the endpoins with HTTP Basic authentication
+    /*TODO: https://www.baeldung.com/spring-security-login*/
+    /*Refer this link and do login*/
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                //HTTP Basic authentication
+                .httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/tenders/**").hasRole("USER")
+                .antMatchers(HttpMethod.POST, "/tenders").hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT, "/books/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.PATCH, "/books/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/books/**").hasRole("ADMIN")
+                .and()
+                .csrf().disable();
+                //.formLogin().disable();]]=
+
+        /*
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/anonymous*").anonymous()
+                .antMatchers("/login*").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login.html")
+                .loginProcessingUrl("/perform_login")
+                .defaultSuccessUrl("/homepage.html", true)
+                //.failureUrl("/login.html?error=true")
+                .failureHandler(authenticationFailureHandler())
+                .and()
+                .logout()
+                .logoutUrl("/perform_logout")
+                .deleteCookies("JSESSIONID")
+                .logoutSuccessHandler(logoutSuccessHandler());
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+            return new BCryptPasswordEncoder();
+        }
+
+         */
+    }
+
+    /*@Bean
+    public UserDetailsService userDetailsService() {
+        //ok for demo
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(users.username("user").password("password").roles("USER").build());
+        manager.createUser(users.username("admin").password("password").roles("USER", "ADMIN").build());
+        return manager;
+    }*/
+}
