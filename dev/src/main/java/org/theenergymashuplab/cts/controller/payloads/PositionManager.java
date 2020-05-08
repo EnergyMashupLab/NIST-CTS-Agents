d6da670b99623565db61959d50bd13cc6f008d86
dhruvin96 <dhruvind66@gmail.com>
2020-05-07 23:37:52 -0400

    Removing temoprary test URI from Position Manager.

--- dev/src/main/java/org/theenergymashuplab/cts/controller/payloads/PositionManager.java
+++ dev/src/main/java/org/theenergymashuplab/cts/controller/payloads/PositionManager.java
@@ -41,7 +41,6 @@ public class PositionManager {
 			@RequestBody PositionAddPayload posPayload,
 			HttpServletResponse response) {
 
-		System.out.println("\n\n\n" + posPayload.toString() + "\n\n\n");
 		// Creating temporary position manager model instance.
 		PositionManagerModel posadd = new PositionManagerModel(
 				positionParty,
@@ -56,7 +55,7 @@ public class PositionManager {
 				posPayload.getInterval().getDuration().getSeconds());
 		
 		logger.info(posadd.toString());
-		System.out.println("\n\n\n" + posPayload.toString() + "\n\n\n");
+
 		if (queryresult.isEmpty()) {
 			// New row to be added.
 			// Saving the position.
@@ -122,84 +121,4 @@ public class PositionManager {
 		return dataList;
 	}
 
-	/*
-	 * GET method /position/{positionParty}/getPosition
-	 *
-	 * GET on this URI returns the position value for positionparty in the interval
-	 * passed
-	 *
-	 * @RequestBody has boundingInterval (in the granularity of the market - one
-	 * hour market it's an interval starting on an hour
-	 *
-	 * Turn the interval into start end time
-	 *
-	 * OK to have one interval and the return is a long
-	 * 
-	 * Future - boundingInterval
-	 */
-
-// position for that interval - if 3 or 4
-	/*
-	 * if UA asks for position bounding interval
-	 * 
-	 * RETURN A LIST <Interval, quantity> each of which fits in the boundingInterval
-	 * 
-	 * What do you do if no positions are there? check for empty at the caller - UA
-	 * does the test of what's back
-	 */
-
-//	position/dumpPositions --print the whole database on the console (System.err)
-
-	/*
-	 * @GetMapping("/test1") public ResponseEntity<Object> getPositionforDuration()
-	 * { long positionParty = 123; Instant sTime =
-	 * Instant.parse("2020-04-30T15:00:00.00Z"); long duration = 3600000;
-	 * 
-	 * List<PositionManagerModel> list =
-	 * posDao.getPositionforDuration(positionParty, sTime, duration); if (list ==
-	 * null) { return ResponseEntity.notFound().build(); } return
-	 * ResponseEntity.ok().body(list); }
-	 * 
-	 * @GetMapping("/testadd/{count}") public PositionManagerModel
-	 * createTestPosition(@PathVariable(value = "count") int count) {
-	 * PositionManagerModel pos = new PositionManagerModel(); Instant sTime = null;
-	 * long duration = 0;
-	 * 
-	 * switch(count) { case 1: // entry 1 duration = 3600000; sTime =
-	 * Instant.parse("2020-04-30T13:00:00.00Z"); pos.setPositionParty(123);
-	 * pos.setTransactionId(456); pos.setStartTime(sTime);
-	 * pos.setEndTime(sTime.plusMillis(duration)); pos.setQuantity(10); break;
-	 * 
-	 * case 2: // entry 1 duration = 3600000; sTime =
-	 * Instant.parse("2020-04-30T14:30:00.00Z"); pos.setPositionParty(123);
-	 * pos.setTransactionId(456); pos.setStartTime(sTime);
-	 * pos.setEndTime(sTime.plusMillis(duration)); pos.setQuantity(20); break;
-	 * 
-	 * case 3: // entry 1 duration = 3600000; sTime =
-	 * Instant.parse("2020-04-30T15:00:00.00Z"); pos.setPositionParty(123);
-	 * pos.setTransactionId(456); pos.setStartTime(sTime);
-	 * pos.setEndTime(sTime.plusMillis(duration)); pos.setQuantity(30); break;
-	 * 
-	 * case 4: // entry 1 duration = 3600000; sTime =
-	 * Instant.parse("2020-04-30T15:30:00.00Z"); pos.setPositionParty(123);
-	 * pos.setTransactionId(456); pos.setStartTime(sTime);
-	 * pos.setEndTime(sTime.plusMillis(duration)); pos.setQuantity(40); break;
-	 * 
-	 * case 5: // entry 1 duration = 3600000; sTime =
-	 * Instant.parse("2020-04-30T17:00:00.00Z"); pos.setPositionParty(123);
-	 * pos.setTransactionId(456); pos.setStartTime(sTime);
-	 * pos.setEndTime(sTime.plusMillis(duration)); pos.setQuantity(50); break;
-	 * 
-	 * case 6: // entry 1 duration = 18000000; sTime =
-	 * Instant.parse("2020-04-30T13:00:00.00Z"); pos.setPositionParty(123);
-	 * pos.setTransactionId(456); pos.setStartTime(sTime);
-	 * pos.setEndTime(sTime.plusMillis(duration)); pos.setQuantity(60); break;
-	 * 
-	 * default:
-	 * 
-	 * break; }
-	 * 
-	 * 
-	 * return posDao.save(pos); }
-	 */
 }
